c7a82b90c7c6f7638bc338861e7e18ce6da8705e
JrnJ <jeroenjochems@outlook.com>
2021-11-23 16:12:59 +0100

    Editted shaders for camera attempt

--- Assets/shaders/chernoo_sprite.fs
+++ Assets/shaders/chernoo_sprite.fs
@@ -2,13 +2,13 @@
 
 layout(location = 0) out vec4 color;
 
-in vec4 v_Position;
+in vec3 v_Position;
 in vec4 v_Color;
 
 uniform sampler2D sprite;
-uniform vec4 spriteColor; // Has to be a uniform vec4
+uniform vec4 spriteColor;
 
 void main()
 {
-    color = spriteColor * texture(sprite, vec2(v_Position.zw));
+    color = spriteColor * texture(sprite, vec2(v_Position.xy));
 }
